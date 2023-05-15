bcda7ad825c2caaffdf19e84c93e188a7b53581f
Joshua Peters <petersjoshuaedu@gmail.com>
2023-05-14 22:53:21 -0400

    idk

--- basic_bot.py
+++ basic_bot.py
@@ -1,3 +1,4 @@
+import time
 import pyautogui
 import keyboard
 
@@ -8,22 +9,38 @@ def print_pixel_information():
 def click(location):
     x, y = location[0], location[1]
     pyautogui.moveTo(x, y)  # move mouse to XY coordinates over num_second seconds
-    pyautogui.click()
+    pyautogui.click(clicks=1)
     pyautogui.moveTo(200,500)
 
 upgrade_rgb = (85,119,255)
 prestige_rgb = (221,68,0)
-location_dict = {"Location 1":(390,200),
-                "Location 2":(390,270),
-                "Location 3":(390,340),
-                "Location 4":(390,410),
-                "Location 5":(390,480),
-                "Location 6":(390,550),
-                "Location 7":(390,620),
-                "Location 8":(390,690),
-                "Location 9":(390,760),
-                "Location 10":(390,830),
-                "Location 11":(390,900)}
+location_dict = {"Location 1":(440,200),
+                "Location 2":(440,265),
+                "Location 3":(440,330),
+                "Location 4":(440,395),
+                "Location 5":(440,460),
+                "Location 6":(440,525),
+                "Location 7":(440,590),
+                "Location 8":(440,655),
+                "Location 9":(440,720),
+                "Location 10":(440,785),
+                "Location 11":(440,850),
+                "Location 12":(440,915)}
+
+'''location_dict = {"Location 1":(440,200),
+                "Location 2":(440,270),
+                "Location 3":(440,340),
+                "Location 4":(440,410),
+                "Location 5":(440,480),
+                "Location 6":(440,550),
+                "Location 7":(440,620),
+                "Location 8":(440,690),
+                "Location 9":(440,760),
+                "Location 10":(440,830),
+                "Location 11":(440,900)}'''
+
+
+paused = True
 
 print("To start program, press [s].")
 keyboard.wait('s')
@@ -31,23 +48,28 @@ print("Starting program...")
 while True:
     #pyautogui.click()
 
-    # Upgrade Buildings    
-    for location in location_dict:
-        #print(f"Checking location {location}...")
-        pixel_r = pyautogui.pixel(location_dict[location][0],location_dict[location][1])[0]
-        if (pixel_r == upgrade_rgb[0]):
-            #print(f"Upgrading location {location}...")
-            click(location_dict[location])
-            continue
-        #print(f"The pixel red value {pixel_r} does not match {upgrade_rgb[0]}")
-        if (pixel_r == prestige_rgb[0]):
-            click(location_dict[location])
-            continue
+    # Upgrade Buildings   
+     
+    if paused == False:
+        for location in location_dict:
+            #print(f"Checking location {location}...")
+            pixel_r = pyautogui.pixel(location_dict[location][0],location_dict[location][1])[0]
+            if (pixel_r == upgrade_rgb[0]):
+                #print(f"Upgrading location {location}...")
+                click(location_dict[location])
+                continue
+            #print(f"The pixel red value {pixel_r} does not match {upgrade_rgb[0]}")
+            if (pixel_r == prestige_rgb[0]):
+                click(location_dict[location])
+                continue
 
-    if (keyboard.is_pressed('d')):
+    if keyboard.is_pressed('p'):
+        paused = paused == False
+        time.sleep(1)
+
+    if keyboard.is_pressed('d'):
         print_pixel_information()
 
-    if (keyboard.is_pressed('q')):
+    if keyboard.is_pressed('q'):
         print("Quiting program...")
         break
-    
